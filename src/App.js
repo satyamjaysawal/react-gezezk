35a7c5b60126c39b3fe37bb7b1e0c86575c5c45f
satyamjaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2022-11-16 15:23:38 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './App.css';
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+ {
+ emoji: '😀',
+ name: "test grinning face"
+ },
+ {
+ emoji: '🎉',
+ name: "party popper"
+ },
+ {
+ emoji: '💃',
+ name: "woman dancing"
+ }
+];
 function App() {
+const greeting = "greeting";
+const displayAction = false;
 return(
-  <>
- <h1>Hello, sunny honney World</h1>
- <p>I am writing JSX</p>
- </>
-)
+ <div className="container">
+ <h1 id={greeting}>Hello, World</h1>
+ {displayAction && <p>I am writing JSX</p>}
+ <ul>
+ {
+ emojis.map(emoji => (
+ <li key={emoji.name}>
+ <button
+ onClick={displayEmojiName}
+ >
+ <span role="img" aria-label={emoji.name}
+ id={emoji.name}>{emoji.emoji}</span>
+ </button>
+ </li>
+ ))
+ }
+ </ul>
+ </div>
+ )
 }
 export default App;
+
