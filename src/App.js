3d9d2277a0ac1cc7172fa7d958a8a1e9e9daf448
satyamjaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2022-11-16 15:41:31 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Instructions from './Instructions.js'
 import './App.css';
 const displayEmojiName = event => alert(event.target.id);
 const emojis = [
@@ -16,28 +17,29 @@ const emojis = [
  }
 ];
 function App() {
-const greeting = "greeting";
-const displayAction = false;
-return(
- <div className="container">
- <h1 id={greeting}>Hello, World</h1>
- {displayAction && <p>I am writing JSX</p>}
- <ul>
- {
- emojis.map(emoji => (
- <li key={emoji.name}>
- <button
- onClick={displayEmojiName}
- >
- <span role="img" aria-label={emoji.name}
- id={emoji.name}>{emoji.emoji}</span>
- </button>
- </li>
- ))
+  const greeting = "greeting";
+  const displayAction = false;
+  return(
+   <div className="container">
+   <h1 id={greeting}>Hello, World</h1>
+   {displayAction && <p>I am writing JSX</p>}
+   <Instructions />
+   <ul>
+   {
+   emojis.map(emoji => (
+   <li key={emoji.name}>
+   <button
+   onClick={displayEmojiName}
+   >
+   <span role="img" aria-label={emoji.name}
+   id={emoji.name}>{emoji.emoji}</span>
+   </button>
+   </li>
+   ))
+  }
+  </ul>
+  </div>
+  )
  }
- </ul>
- </div>
- )
-}
-export default App;
-
+ export default App;
+ 
---
+++ src/Instructions.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export class Instructions extends Component {
+render() {
+ return(
+ <p>Click on an emoji to view the emoji short name.</p>
+ )
+}
+}
