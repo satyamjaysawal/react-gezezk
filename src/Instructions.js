9dda2a5d4327209d09fbe92923a19363b81eaffa
satyamjaysawal <108862706+satyamjaysawal@users.noreply.github.com>
2022-11-16 16:58:04 +0530

    1 file modified

--- src/Instructions.js
+++ src/Instructions.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-export class Instructions extends Component {
-render() {
- return(
- <p>Click on an emoji to view the emoji short name.</p>
- )
-}
+export default class Instructions extends Component {
+  render() {
+    return <p>Click on an emoji to view the emoji short name.</p>;
+  }
 }
